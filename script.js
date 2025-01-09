ee07ea1d1d47637dc2d8b079f39dcbc6bae6846e
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2025-01-09 15:33:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,20 @@ window.addEventListener('scroll', function () {
   const menu = document.getElementById('nav-menu');
   menu.classList.remove('active');
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+    // Toggle Dark Mode
+    darkModeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+
+        // Change icon for the toggle button
+        if (document.body.classList.contains("dark-mode")) {
+            darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+        } else {
+            darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+        }
+    });
+});
